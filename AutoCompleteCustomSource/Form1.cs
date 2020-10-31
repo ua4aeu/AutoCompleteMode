11a70c2abcefce64e28b6a32138eadad1b403e11
ua4aeu <ra4a@yandex.ru>
2020-10-31 12:55:54 +0300

    Add files via upload

---
+++ AutoCompleteCustomSource/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoCompleteCustomSource
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var source = new AutoCompleteStringCollection();
+            source.AddRange(new string[]
+                            {
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December"
+                            });
+
+            // Create and initialize the text box.
+            var textBox = new TextBox
+            {
+                AutoCompleteCustomSource = source,
+                AutoCompleteMode =
+                                  AutoCompleteMode.SuggestAppend,
+                AutoCompleteSource =
+                                  AutoCompleteSource.CustomSource,
+                Location = new Point(20, 20),
+                Width = ClientRectangle.Width - 40,
+                Visible = true
+            };
+
+            // Add the text box to the form.
+            Controls.Add(textBox);
+        }
+    }
+}
